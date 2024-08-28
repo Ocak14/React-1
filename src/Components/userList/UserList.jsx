a69f57123479e668205c33d16df374b32cfa11aa
Ocak14 <sayfullayeva191@gmail.com>
2024-08-28 11:41:21 +0500

    UserList.jsx

--- src/Components/userList/UserList.jsx
+++ src/Components/userList/UserList.jsx
@@ -9,11 +9,11 @@ const UserList = ({ users, deleteUser }) => {
             <div className='card' key={user.id}>
               <div className="card-inner">
                 <img src={user.image} alt={user.name} width={170} height={170} />
-                <h3>{user.firstName} {user.lastName} {user.age} yosh</h3>
-                <p>Qayerdan: {user.from} </p>
-                <p>Kasbi: {user.job} </p>
-                <p>Jinsi: {user.gender}</p>
-                <button onClick={() => deleteUser(user.id)}>O'chirish</button>
+                <h3>{user.firstName} {user.lastName} {user.age} Age</h3>
+                <p>From: {user.from} </p>
+                <p>Job: {user.job} </p>
+                <p>Gender: {user.gender}</p>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
               </div>
             </div>
           )
